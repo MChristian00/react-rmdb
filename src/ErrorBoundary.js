9ddd8d20aa0965156249903e39eec8c2bbad4335
MCHristian00 <manzichristian07@gmail.com>
2022-06-27 18:34:21 -0500

    Added Error Boundary
    Added Error boundary file and enclosed APP in it

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: "", errorInfo: "" };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+    this.setState({
+      hasError: true,
+      error,
+      errorInfo
+    })
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }  
+}
